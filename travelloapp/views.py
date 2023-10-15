58f68229302fd1ca9e67e4ea9cf618692b2ddae7
Sams <shams.iqbal@g.bracu.ac.bd>
2023-10-15 22:12:53 +0600

    Just before I pass the objects through list and for loop

--- travelloapp/views.py
+++ travelloapp/views.py
@@ -8,4 +8,29 @@ def index(request):
     dest1.desc = "The heart of Bangladesh"
     dest1.price = 800
 
-    return render(request, 'index.html', {'dest1' : dest1})
+    dest2 = Destination()
+    dest2.name = 'Chittagong'
+    dest2.desc = 'The mother city of Ocean'
+    dest2.price = '700'
+
+    dest3 = Destination()
+    dest3.name = 'Kushtia'
+    dest3.desc = 'The city of tradition'
+    dest3.price = '600'
+
+    dest4 = Destination()
+    dest4.name = 'Khulna'
+    dest4.desc = 'City Beside Rupsha'
+    dest4.price = '650'
+
+    dest5 = Destination()
+    dest5.name = 'Rajshahi'
+    dest5.desc = 'City of Mango'
+    dest5.price = '650'
+
+    dest6 = Destination()
+    dest6.name = 'Sylhet'
+    dest6.desc = 'City of Hills, waterfalls'
+    dest6.price = '700'
+
+    return render(request, 'index.html', {'dest1' : dest1, 'dest2' : dest2, 'dest3': dest3, 'dest4': dest4, 'dest5': dest5, 'dest6': dest6})
