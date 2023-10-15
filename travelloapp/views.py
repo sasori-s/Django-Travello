05a47ee46ee5e2048f4f0e0c1391b19e6d3706d2
Sams <shams.iqbal@g.bracu.ac.bd>
2023-10-15 19:36:42 +0600

    changed one destination to Dhaka

--- travelloapp/views.py
+++ travelloapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.name = 'Dhaka'
+    dest1.desc = "The heart of Bangladesh"
+    dest1.price = 800
+
+    return render(request, 'index.html', {'dest1' : dest1})
